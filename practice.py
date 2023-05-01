5945a8179568082faafe5e1c25e04c705414f937
aeyongworld <rlaehdehd2001@gmail.com>
2023-05-01 22:35:53 +0900

    섹션8의 다중상속까지

--- practice.py
+++ practice.py
@@ -720,3 +720,297 @@ print(type(answer))
 """
 
 
+""" [다양한 출력포맷]
+
+# 빈 자리는 빈공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
+print("{0: >10}".format(500))
+# 양수일 땐 +로 표시, 음수일 땐 -로 표시
+print("{0: >+10}".format(500))
+print("{0: >+10}".format(-500))
+# 왼쪽 정렬하고, 빈칸으로 _로 채움
+print("{0:_<10}".format(500))
+# 3자리 마다 콤마를 찍어주기
+print("{0:,}".format(100000000000))
+# 3자리 마다 콤마를 찍어주기, +- 부호도 붙이기
+print("{0:+,}".format(100000000000))
+print("{0:+,}".format(-100000000000))
+# 3자리 마다 콤마를 찍어주기, 부호도 붙이고, 자릿수 확보하기
+# 돈이 많으면 행복하니까 빈 자리는 ^ 로 채워주기
+print("{0:^<+30,}".format(100000000000))
+# 소수점 출력
+print("{0:f}".format(5/3))
+# 소수점 특정 자리수 까지만 표시 (소수점 3째 자리에서 반올림)
+print("{0:.2f}".format(5/3))
+
+"""
+
+
+""" [파일입출력]
+
+score_file = open("score.txt", "w", encoding="utf8")
+print("수학 : 0", file=score_file)
+print("영어 : 50", file=score_file)
+score_file.close()
+
+score_file = open("score.txt", "a", encoding="utf8")
+score_file.write("과학 : 80")
+score_file.write("\n코딩 : 100")
+score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8")
+print(score_file.read())
+score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8")
+print(score_file.readline(), end="") # 줄별로 읽기, 한 줄 읽고 커서는 다음 줄로 이동
+print(score_file.readline(), end="")
+print(score_file.readline(), end="")
+print(score_file.readline(), end="")
+score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8")
+while True:
+    line = score_file.readline()
+    if not line:
+        break
+    print(line, end="")
+score_file.close()
+
+
+score_file = open("score.txt", "r", encoding="utf8")
+lines = score_file.readlines() # list 형태로 저장
+for line in lines:
+    print(line, end="")
+
+score_file.close()
+
+"""
+
+
+""" [pickle]
+
+import pickle
+profile_file = open("profile.pickle", "wb")
+profile = {"이름":"박명수", "나이":30, "취미":["축구", "골프", "코딩"]}
+print(profile)
+pickle.dump(profile, profile_file) # profile 에 있는 정보를 file 에 저장
+profile_file.close()
+
+profile_file = open("profile.pickle", "rb")
+profile = pickle.load(profile_file) # file 에 있는 정보를 profile 에 불러오기
+print(profile)
+profile_file.close()
+
+"""
+
+
+""" [with]
+
+import pickle
+
+with open("profile.pickle", "rb") as profile_file:
+    print(pickle.load(profile_file))
+    
+with open("study.txt", "w", encoding="utf8") as study_file:
+    study_file.write("파이썬을 열심히 공부하고 있어요")
+
+with open("study.txt", "r", encoding="utf8") as study_file:
+    print(study_file.read())
+
+"""
+
+
+
+# 섹션8
+
+""" [클래스]
+
+# # 마린 : 공격 유닛, 군인. 총을 쏠 수 있음
+
+# name = "마린" # 유닛의 이름
+# hp = 40 # 유닛의 체력
+# damage = 5 # 유닛의 공격력
+
+# print("{0} 유닛이 생성되었습니다.".format(name))
+# print("체력 {0}, 공격력 {1}\n".format(hp, damage))
+
+# # 탱크 : 공격 유닛, 탱크. 포를 쏠 수 있는데, 일반 모드 / 시즈 모드.
+# tank_name = "탱크"
+# tank_hp = 150
+# tank_damage = 35
+
+# print("{0} 유닛이 생성되었습니다.".format(tank_name))
+# print("체력 {0}, 공격력 {1}\n".format(tank_hp, tank_damage))
+
+# tank2_name = "탱크"
+# tank2_hp = 150
+# tank2_damage = 35
+
+# print("{0} 유닛이 생성되었습니다.".format(tank2_name))
+# print("체력 {0}, 공격력 {1}\n".format(tank2_hp, tank2_damage))
+
+# def attack(name, location, damage):
+#     print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format(name, location, damage))
+    
+# attack(name, "1시", damage)
+# attack(tank_name, "1시", tank_damage)
+# attack(tank2_name, "1시", tank2_damage)
+
+
+class Unit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+marine1 = Unit("마린", 40, 5)
+marine2 = Unit("마린", 40, 5)
+tank = Unit("탱크", 150, 35)
+
+"""
+
+
+""" [멤버변수]
+
+class Unit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+        
+# 레이스 : 공중 유닛, 비행기. 클로킹 (상대방에게 보이지 않음)
+wraith1 = Unit("레이스", 80, 5)
+print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
+
+# 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+wraith2 = Unit("빼앗은 레이스", 80, 5)
+wraith2.clocking = True
+
+if wraith2.clocking == True:
+    print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
+
+"""
+
+
+""" [메소드]
+
+class Unit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+
+class AttackUnit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(self.name, location, self.damage))
+        
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+            
+
+# 파이어뱃 : 공격 유닛, 화염방사기.
+firebat1 = AttackUnit("파이어뱃", 50, 16)
+firebat1.attack("5시")
+
+#  공격 2번 받는다고 가정
+firebat1.damaged(25)
+firebat1.damaged(25)
+
+"""
+
+
+""" [상속]
+
+# 일반 유닛
+class Unit:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+# 공격 유닛
+class AttackUnit(Unit):
+    def __init__(self, name, hp, damage):
+        Unit.__init__(self, name, hp)
+        self.damage = damage
+        
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(self.name, location, self.damage))
+        
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+# 메딕 : 의무병
+
+"""
+
+
+
+# 일반 유닛
+class Unit:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+# 공격 유닛
+class AttackUnit(Unit):
+    def __init__(self, name, hp, damage):
+        Unit.__init__(self, name, hp)
+        self.damage = damage
+        
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(self.name, location, self.damage))
+        
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+# 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃 / 탱크 등을 수송. 공격 X
+
+# 날 수 있는 기능을 가진 클래스
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+    
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]".format(name, location, self.flying_speed))
+        
+
+# 공중 공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
+
+
+# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
+
+
+
+
+
+
