af540142e2ff19f28f87e1a7ce93bbbe4db82925
kimju0714 <rlaehdehd2001@gmail.com>
2023-05-01 12:42:40 +0900

    섹션7 표준입출력까지만

--- practice.py
+++ practice.py
@@ -695,3 +695,28 @@ print("남은 총 : {0}".format(gun))
 
 
 
+# 섹션7
+
+""" [표준입출력]
+# import sys
+# print("Python", "Java", file=sys.stdout)
+# print("Python", "Java", file=sys.stderr)
+
+# scores = {"수학":0, "영어":50, "코딩":100}
+# for subject, score in scores.items():
+#     # print(subject, score)
+#     print(subject.ljust(8), str(score).rjust(4), sep=":")
+
+# 은행 대기순번표
+# 001, 002, 003, ...
+# for num in range(1, 21):
+#     print("대기번호 : " + str(num).zfill(3))
+
+
+# answer = input("아무 값이나 입력하세요 : ")
+answer = 10
+print(type(answer))
+# print("입력하신 값은 " + answer + "입니다.")
+"""
+
+
